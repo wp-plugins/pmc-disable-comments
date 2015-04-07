a02a15b5411bdb5c8d398562618f2c08b9dabf13
pmcdotcom <pmcdotcom@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-07 20:13:00 +0000

    Update for Wordpress 4.1 compatibility
    git-svn-id: https://plugins.svn.wordpress.org/pmc-disable-comments/trunk@1129874 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- pmc-disable-comments.php
+++ pmc-disable-comments.php
@@ -3,18 +3,51 @@
 Plugin Name: PMC Disable Comments
 Plugin URI: https://github.com/vickybiswas/pmc-disable-comments
 Description: PMC Disable allows you to switch default commenting on/off for individual post types.
-Version: 1.0.1
+Version: 1.1.0
 Author: Vicky Biswas
 Author URI: http://www.pmc.com/
 License: GPLv2
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 */
 
-abstract class PMC_Disable_Comments {
+class PMC_Disable_Comments {
 	private static $blocked_types;
-	public function admin() {
+	public function __construct(){
+		add_action( 'admin_init', array('PMC_Disable_Comments','admin') );
+		add_action( 'wp_loaded', array( $this, 'setup_filters' ) );
 		self::$blocked_types = (array) get_option( 'pmc-disable-comments-toggle' );
-		add_filter( 'default_content', array('PMC_Disable_Comments','default_comment'), 10, 2 );
+	}
+	public function setup_filters(){
+		if( !is_admin() ) {
+			add_action( 'template_redirect', array( $this, 'load_comment_template' ) );
+		}
+	}
+	/**
+	 * Checks if current post type is in out black list
+	 */
+	function load_comment_template() {
+		if( !is_singular() ){
+			return;
+		}
+		if( in_array( get_post_type(), self::$blocked_types ) ) {
+			add_filter( 'comments_template', array( $this, 'dummy_comments_template' ), 20 );
+			// Remove comment-reply script for themes that include it indiscriminately
+			wp_deregister_script( 'comment-reply' );
+			// feed_links_extra inserts a comments RSS link
+			remove_action( 'wp_head', 'feed_links_extra', 3 );
+		}
+	}
+	/**
+	 * Returns dummy comment template path
+	 * @return string
+	 */
+	function dummy_comments_template() {
+		return dirname( __FILE__ ) . '/comments-template.php';
+	}
+	/**
+	 * Register admin option
+	 */
+	public function admin() {
 		add_settings_section(
 			'pmc-disable-comments',
 			'Disable/Enable comments for the below:',
@@ -33,26 +66,24 @@ abstract class PMC_Disable_Comments {
 			'pmc-disable-comments-toggle'
 		);
 	}
-	function default_comment( $content, $post ) {
-		if ( in_array( $post->post_type, self::$blocked_types ) ) {
-			$post->comment_status = false;
-			$post->ping_status = false;
-		}
-		return $content;
-	}
+	/**
+	 * Register admin form fields
+	 *
+	 * @param $args
+	 */
 	function admin_settings( $args ) {
 		$post_types=get_post_types('','objects');
 		echo '<fieldset>';
-		foreach ($post_types  as $post_type_slug=>$post_type ) {
+		foreach ($post_types as $post_type_slug=>$post_type ) {
 			if ( post_type_supports( $post_type_slug, 'comments' ) ) {
 				echo "
-				<label for='pmc-disable-comments-$post_type_slug'>
-					<input type='checkbox' id='pmc-disable-comments-$post_type_slug' name='pmc-disable-comments-toggle[]' value='$post_type_slug' " . checked(in_array($post_type_slug,self::$blocked_types),true,false) . " />{$post_type->labels->singular_name}
-					<br />
-				</label>";
+<label for='pmc-disable-comments-$post_type_slug'>
+<input type='checkbox' id='pmc-disable-comments-$post_type_slug' name='pmc-disable-comments-toggle[]' value='$post_type_slug' " . checked(in_array($post_type_slug,self::$blocked_types),true,false) . " />{$post_type->labels->singular_name}
+<br />
+</label>";
 			}
 		}
 		echo '</fieldset>';
 	}
 }
-add_action( 'admin_init', array('PMC_Disable_Comments','admin') );
+new PMC_Disable_Comments();
